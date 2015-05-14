f02cb077f09bc9d935f730e5e42b88f98198f87d
Gabriele Baldassarre <gabriele.baldassarre@mrm-mccann.com>
2015-05-14 17:19:31 +0200

    Full vector and pagination support

---
+++ Rfacebook/R/getPosts.R
@@ -0,0 +1,198 @@
+#' @rdname getPosts
+#' @export
+#'
+#' @title 
+#' Extract information about a set of public Facebook posts
+#'
+#' @description
+#' \code{getPosts} retrieves information about a set of public Facebook posts, including
+#' list of comments and likes.
+#'
+#' @details 
+#' \code{getPosts} returns a list with three components: \code{posts}, 
+#' \code{likes}, and \code{comments}. First, \code{posts} contains information
+#' about the posts: author, creation date, id, counts of likes, comments, and 
+#' shares, etc. Second, \code{likes} is a data frame that contains names and
+#' Facebook IDs of all the users that liked the post. Finally, \code{comments}
+#' is a data frame with information about the comments to the post (author, 
+#' message, creation time, id).
+#'
+#' @author
+#' Gabriele Baldassarre \email{gabriele@gabrielebaldassarre.com}
+#' @seealso \code{\link{getUsers}}, \code{\link{getPage}}, \code{\link{fbOAuth}}
+#'
+#' @param posts A vector or a comma-delimited string of post IDs
+#'
+#' @param token Either a temporary access token created at
+#' \url{https://developers.facebook.com/tools/explorer} or the OAuth token 
+#' created with \code{fbOAuth}.
+#'
+#' @param n numeric, maximum number of comments and likes to return.
+#'
+#' @param n.likes numeric, maximum number of likes to return. Default is \code{n}.
+#'
+#' @param n.comments numeric, maximum number of likes to return. Default is 
+#' \code{n}.
+#' 
+#' @param fields A vector or a comma-delimited strings of fields to get 
+#' \code{n}.
+#'
+#' @examples \dontrun{
+#' ## See examples for fbOAuth to know how token was created.
+#' ## Getting information about Facebook's Facebook Page
+#'	load("fb_oauth")
+#'	fb_page <- getPage(page="facebook", token=fb_oauth)
+#' ## Getting information and likes/comments about most recent post
+#'	posts <- getPosts(post=fb_page$id[1:10], n=2000, token=fb_oauth)
+#' }
+#'
+
+getPosts <- function(posts, token, n=500, n.likes=n, n.comments=n, fields = "id,from,message,created_time,type,link,name,shares"){
+  
+  post.fields <- paste0(unique(
+    unlist(strsplit(fields, split = ","))),
+    collapse = ","
+  )
+  posts.v <- unique(unlist(strsplit(posts, split = ",")))
+  posts.f <- rep(seq_len(ceiling(length(posts.v) / 50)),each = 50,length.out = length(posts.v))
+  posts.chunks <- split(posts.v, f = posts.f)
+
+  if(length(posts.chunks) > 1){
+    
+    do.call(rbind,
+            lapply(posts.chunks, function(single.chunk) {
+                getPosts(posts = single.chunk, token = token, n = n, n.likes = n.likes, n.comments = n, fields = fields)
+      
+    })
+    )
+    
+  }
+  
+  else {
+  
+  url <- paste0(
+    "https://graph.facebook.com/v2.3/?ids=",
+    paste0(posts.v, collapse = ","),
+    "&fields=", post.fields,
+    ",comments.summary(true)",
+    ifelse(n.comments > 0, paste0(".fields(id,from,message,created_time,like_count).limit(", ifelse(n.comments > 1000, 1000, n.comments), ")"), ".limit(0)"),
+    ",likes.summary(true)",
+    ifelse(n.likes > 0, paste0(".fields(id,name,profile_type).limit(", ifelse(n.likes > 1000, 1000, n.likes), ")"), ".limit(0)")
+  )
+  
+  content <- callAPI(url=url, token=token)
+  
+  # Check for permission
+  if (length(content)==0){ 
+    stop("You're not authorized to get this information, Please check your permissions before retrying.")
+  }  
+  # error traps: retry 3 times if error
+  error <- 0
+  while (length(content$error_code)>0){
+    Sys.sleep(0.5)
+    error <- error + 1
+    content <- callAPI(url=url, token=token)		
+    if (error==3){ stop(content$error_msg) }
+  }
+  
+  
+  all.Posts <- do.call(rbind.fill,
+                            lapply(content, function(sublist) {
+                              postDataToDF(sublist, post.fields)
+                            })
+  )
+  
+  
+  #   if (n.likes > 0) {
+  #     out[["likes"]] <- do.call(rbind.fill,
+  #                               lapply(content, function(sublist) {
+  #                                 l <- likesDataToDF(sublist$likes$data)
+  #                                 if(length(l) > 0) {
+  #                                   l$post.id <- sublist$id
+  #                                 }
+  #                                 return(l)
+  #                               })
+  #     )
+  #     
+  #   }
+  #   
+  
+  # Likes
+  if (n.likes > 0) {
+    all.Likes <- do.call(rbind.fill,
+                              lapply(content, function(sublist) {
+                                page <- 0
+                                l <- data.frame()
+                                total.likes <- 0
+                                
+                                repeat {
+                                  likedata <- NULL
+                                  if(page == 0){
+                                    likedata <- sublist$likes
+                                  } else {
+                                    likedata <- callAPI(url=next.url, token=token)
+                                  }
+                                  next.url <- likedata$paging$`next`
+                                  
+                                  l.page <- likesDataToDF(likedata$data)
+                                  if(length(l.page) > 0) {
+                                    l.page$post.id <- sublist$id
+                                    l <- rbind.fill(l, l.page)
+                                    page <- page + 1
+                                    total.likes <- total.likes + nrow(l.page)
+                                    
+                                  }
+                                  
+                                  if(total.likes >= n.likes | is.null(next.url)){
+                                    return(head(l, n.likes))
+                                  }
+                                  
+                                }
+                              }
+                              )
+    )
+    
+  }
+  
+  if (n.comments > 0) {
+    all.Comments <- do.call(rbind.fill,
+                                 lapply(content, function(sublist) {
+                                   page <- 0
+                                   c <- data.frame()
+                                   total.comments <- 0
+                                   
+                                   repeat {
+                                     commentdata <- NULL
+                                     if(page == 0){
+                                       commentdata <- sublist$comments
+                                     } else {
+                                       commentdata <- callAPI(url=next.url, token=token)
+                                     }
+                                     next.url <- commentdata$paging$`next`
+                                     
+                                     c.page <- commentsDataToDF(commentdata$data)
+                                     if(length(c.page) > 0) {
+                                       c.page$post.id <- sublist$id
+                                       c <- rbind.fill(c, c.page)
+                                       page <- page + 1
+                                       total.comments <- total.comments + nrow(c.page)
+                                       
+                                     }
+                                     
+                                     if(total.comments >= n.comments | is.null(next.url)){
+                                       return(head(c, n.comments))
+                                     }
+                                     
+                                   }
+                                 }
+                                 )
+    )
+    
+  }
+  
+  fb.c <- fb.Corpus(all.Posts, all.Likes, all.Comments)
+  #fb.c[["raw"]] <- content
+  
+  return(fb.c)
+  }
+}
