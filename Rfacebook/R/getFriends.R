cce61e846a09102a6ef17195641022d61e3ede08
Gabriele Baldassarre <gabriele.baldassarre@mrm-mccann.com>
2015-05-18 20:02:30 +0200

    vectorized version

--- Rfacebook/R/getFriends.R
+++ Rfacebook/R/getFriends.R
@@ -15,78 +15,123 @@
 #' token to query the API will be returned.
 #'
 #' @author
-#' Pablo Barbera \email{pablo.barbera@@nyu.edu}
+#' Gabriele Baldassarre \email{gabriele@@gabrielebaldassarre.com}
 #' @seealso \code{\link{getUsers}}, \code{\link{fbOAuth}}
 #'
 #' @param token Either a temporary access token created at
 #' \url{https://developers.facebook.com/tools/explorer} or the OAuth token 
 #' created with \code{fbOAuth}.
 #'
-#' @param simplify If \code{TRUE}, function will return only name and id for each friend.
-#' If \code{FALSE}, it will return additional information from their profiles: gender, birthday,
-#' location, hometown, relationship status and profile picture. 
+#' @param fields vector or comma-separated string of fields to get
+#'
 #'
 #' @examples \dontrun{
 #' ## Copy and paste token created at FB Graph API Explorer
 #'  token <- "XXXXXX"
-#'	my_friends <- getFriends(token=token, simplify=TRUE)
+#'	my_friends <- getFriends(token=token)
 #' ## Since users are ordered by ID, this will return 10 oldest user accounts
 #'	head(my_friends, n=10)
 #' }
 #'
 
-getFriends <- function(token, simplify=FALSE){
-	
-	tkversion <- getTokenVersion(token)
-
-	if (tkversion=="v2"){
-		message("Only friends who use the application will be returned\n",
-			"See ?getFriends for more details")
-		base_url <- 'https://graph.facebook.com/v2.0'
-	}
-
-	if (tkversion=="v1"){
-		base_url <- 'https://graph.facebook.com/'
-	}
-
-	if (simplify==TRUE){
-		query <- paste0(base_url, '/me/friends?limit=5000')
-		content <- callAPI(query, token)
-		if (length(content$data)==0){
-			stop("No friend information is available. See ?getFriends for more details.")
-		}
-		error <- tryCatch(friends <- matrix(unlist(content$data), ncol=2, byrow=TRUE),
-			error=function(e) e)
-		if (inherits(error, 'error')){
-			stop("No friend information is available. See ?getFriends for more details.")
-		}
-		while (length(content$data)>0){
-			query <- content$paging$`next`
-			content <- callAPI(query, token)
-			if (length(content$data)>0){
-				friends <- rbind(friends, matrix(unlist(content$data), ncol=2, byrow=TRUE))
-			}
-		}
-		friends <- data.frame(friends, stringsAsFactors=F)
-		names(friends) <- c("name", "id")
-	}	
-
-	if (simplify==FALSE){
-		query <- paste(base_url, "/me/friends?",
-			"fields=id,name,first_name,last_name,gender,locale,birthday,location,",
-			"hometown,relationship_status,picture.type(large)&limit=100", sep="")
-		content <- callAPI(query, token)
-		if (length(content$data)==0){
-			stop("No friend information is available.")
-		}
-		friends <- userDataToDF(content$data, private_info=TRUE)
-		while (length(content$data)>0){
-			query <- content$paging$`next`
-			content <- callAPI(query, token)
-			if (length(content$data)>0){
-				friends <- rbind(friends, userDataToDF(content$data, private_info=TRUE))
-			}
-		}
-	}	
-	return(friends)
+getFriends <- function(token, users="me", fields = "id,name"){
+  
+  details.pagination.define <- 500
+  friends.pagination.define <- 10  
+  
+  if (getTokenVersion(token)=="v1"){
+    stop("Calls using v1 token is no longer available.")
+  }
+  
+  friends.fields <- paste0(unique(
+    unlist(strsplit(fields, split = ","))),
+    collapse = ","
+  )
+  
+  friends.v <- unique(unlist(strsplit(users, split = ",")))
+  friends.f <- rep(seq_len(ceiling(length(friends.v) / friends.pagination.define)),each = friends.pagination.define,length.out = length(friends.v))
+  friends.chunks <- split(friends.v, f = friends.f)
+  
+  if(length(friends.chunks) > 1){
+    
+    do.call(rbind,
+            lapply(friends.chunks, function(single.chunk) {
+              getFriends(token = token, users = single.chunk, fields = fields)
+              
+            })
+    )
+    
+  }
+  
+  else {
+    
+    url <- paste0(
+      "https://graph.facebook.com/v2.3/?ids=",
+      paste0(friends.v, collapse = ","),
+      "&fields=id,name,friends.summary(false){", friends.fields, "}"
+    )
+    
+    content <- callAPI(url=url, token=token)
+    
+    # Check for permission
+    if (length(content)==0){ 
+      stop("You're not authorized to get this information, Please check your permissions before retrying.")
+    }  
+    # error traps: retry 3 times if error
+    error <- 0
+    
+    while (length(content$error_code)>0){
+      Sys.sleep(1)
+      error <- error + 1
+      content <- callAPI(url=url, token=token)  	
+      if (error==3){ stop(content$error_msg) }
+    }
+    
+    # Friends
+    all.Friends <- do.call(rbind.fill,
+                           lapply(content, function(sublist) {
+                             page <- 0
+                             f <- data.frame()
+                             total.friends <- 0
+                             
+                             # TODO: make a better log
+                             cat(paste("\nGetting ", sublist$name, "'s friends using this app", sep = ""))
+                             
+                             repeat {
+                               friendsdata <- NULL
+                               if(page == 0){
+                                 friendsdata <- sublist$friends
+                               } else {
+                                 friendsdata <- callAPI(url=next.url, token=token)
+                               }
+                               next.url <- friendsdata$paging$`next`
+                               
+                               f.page <- detailsDataToDF(friendsdata$data, fields = friends.fields)
+                               if(!is.null(f.page) && nrow(f.page) > 0) {
+                                 # TODO: use detailsDataToToDF here instead
+                                 f.page$parent.id <- sublist$id
+                                 f.page$parent.name <- sublist$name
+                                 f <- rbind.fill(f, f.page)
+                                 page <- page + 1
+                                 total.friends <- total.friends + nrow(f.page)
+                                 
+                               }
+                               
+                               if(is.null(next.url)){
+                                 cat("...Done!\n")
+                                 return(f)
+                               }
+                               
+                               # Graceful waiting before next call
+                               Sys.sleep(0.5)
+                               
+                             }
+                           }
+                           )
+    )
+    
+    
+    return(all.Friends)
+  }
+  
 }
