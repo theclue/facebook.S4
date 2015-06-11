db7fb2df85e8befbd6926a8ae4b09b0062f4cc44
Gabriele Baldassarre <gabriele.baldassarre@mrm-mccann.com>
2015-06-11 17:23:28 +0200

    new naming convention & helper functions

--- facebook.advanced/R/facebook.permissions.R
+++ facebook.advanced/R/facebook.permissions.R
@@ -1,14 +1,15 @@
-#' @rdname getPermissions
+#' @rdname facebook.permissions
 #' @export
 #'
 #' @title 
 #' Extract information about the permissions for the current user
 #'
 #' @description
-#' \code{getPermissions} retrieves informations about the permissions the current user actually has.
+#' \code{facebook.permissions} retrieves informations about the permissions the current user actually has with the
+#' current application.
 #'
 #' @details 
-#' \code{getPermissions} returns a character vector where each row is an action the authenticated user is granted to perform
+#' \code{facebook.permissions} returns a character vector where each row is an action the authenticated user is granted to perform
 #' through Facebook Graph API with the current token.
 #'
 #' @author
@@ -23,14 +24,13 @@
 #' ## See examples for fbOAuth to know how token was created.
 #' ## Getting information about current user's permissions.
 #'	load("fb_oauth")
-#'	me.permissions <- getPermissions(token=fb_oauth)
+#'	me.permissions <- facebook.permissions(token=fb_oauth)
 #' }
 #'
-
-getPermissions <- function(token){
+facebook.permissions <- function(token){
   
-  url <- "https://graph.facebook.com/v2.3/?ids=me&fields=permissions"
-  content <- callAPI(url=url, token=token)
+  query <- "?ids=me&fields=permissions"
+  content <- facebook.query(query = query, token = token)
   
   permissions <- sapply(p$me$permissions$data, function(x) { if(x$status == "granted") return(x$permission) })
   
