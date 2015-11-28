0b88e0661e88cb8fff69694e9c28aff4ff20bec8
Gabriele Baldassarre <gabriele@gabrielebaldassarre.com>
2015-11-28 01:02:54 +0100

    remove test files

--- as.data.frame_FacebookPageset_logical_logical.R
+++
@@ -1,7 +0,0 @@
-setMethod("as.data.frame",
-    signature(x = "FacebookPageset", row.names = "logical", optional = "logical"),
-    function (x, row.names = NULL, optional = FALSE, ...) 
-    {
-        stop("need a definition for the method here")
-    }
-)
--- as.list_FacebookPageset.R
+++
@@ -1,7 +0,0 @@
-setMethod("as.list",
-    signature(x = "FacebookPageset"),
-    function (x, ...) 
-    {
-        stop("need a definition for the method here")
-    }
-)
